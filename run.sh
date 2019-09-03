9e104b07569bc8cf62b442bd883ca9422948797d
Radosław Szmit <radoslawszmit@gmail.com>
2019-09-03 11:35:10 +0200

    Spark training

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+git pull; git push; git status
+mvn clean install
+
+cd ../spark-training
+git pull; git push; git status
+mvn clean install
+
+cd ../kafka-training
+git pull; git push; git status
+mvn clean install
+
+cd ../aws-big-data-ai-training
+git pull; git push; git status
+mvn clean install
