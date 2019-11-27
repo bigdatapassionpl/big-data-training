9ca7caf6609f021459f3c6dfc910262eebda8d27
Radosław Szmit <radoslawszmit@gmail.com>
2019-11-27 01:29:53 +0100

    Compilation script

--- run.sh
+++ run.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 git pull; git push; git status
-mvn clean install
+mvn clean install -DskipTests=true
 
 cd ../hadoop-training
 git pull; git push; git status
-mvn clean install
+mvn clean install -DskipTests=true
 
 cd ../hbase-training
 git pull; git push; git status
-mvn clean install
+mvn clean install -DskipTests=true
 
 cd ../spark-training
 git pull; git push; git status
-mvn clean install
+mvn clean install -DskipTests=true
 
 cd ../kafka-training
 git pull; git push; git status
-mvn clean install
+mvn clean install -DskipTests=true
 
 cd ../aws-big-data-ai-training
 git pull; git push; git status
-mvn clean install
+mvn clean install -DskipTests=true
