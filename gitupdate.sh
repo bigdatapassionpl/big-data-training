5226a1066a81bc5f887d472f558050bf09ed5549
Radosław Szmit <radoslawszmit@gmail.com>
2020-06-04 17:08:59 +0200

    Spark version

---
+++ gitupdate.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+git pull; git push; git status
+
+cd ../hadoop-training
+git pull; git push; git status
+
+cd ../hbase-training
+git pull; git push; git status
+
+cd ../spark-training
+git pull; git push; git status
+
+cd ../kafka-training
+git pull; git push; git status
+
+cd ../aws-big-data-ai-training
+git pull; git push; git status
