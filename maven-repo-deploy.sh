91f4d8657b421d5eca4f2232eb067de68af93c71
Radosław Szmit <radoslaw.szmit@getindata.com>
2022-11-12 16:51:38 +0100

    Adding Maven Repository

--- maven-repo-deploy.sh
+++ maven-repo-deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 mvn deploy:deploy-file \
-  "-DgroupId=com.bigdatapassion" \
-  "-DartifactId=big-data-training" \
-  "-Dversion=1.0-SNAPSHOT" \
-  "-Dpackaging=pom" \
-  "-Dfile=pom.xml" \
-  "-DrepositoryId=radek-maven-repo-snapshot" \
-  "-Durl=s3://radek-maven-repo/snapshot"
+  -DgroupId=com.bigdatapassion \
+  -DartifactId=big-data-training \
+  -Dversion=1.0-SNAPSHOT \
+  -Dpackaging=pom \
+  -Dfile=pom.xml \
+  -DpomFile=pom.xml \
+  -DrepositoryId=radek-maven-repo-snapshot \
+  -Durl=s3://radek-maven-repo/snapshot
